0493f53d01b1a563097abbd3191e6069ca080a49
Hang <583175694@qq.com>
2021-01-17 22:31:32 +0800

    first commit

---
+++ src/pages/Template/index.tsx
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+import './index.css'
+import { connect } from 'react-redux'
+import { RootState } from '../../core/reducers'
+import { ThunkDispatch } from 'redux-thunk'
+import { AnyAction } from 'redux'
+
+type StateProps = {}
+
+type DispatchProps = {}
+
+type PageOwnProps = {}
+
+type IProps = StateProps & DispatchProps & PageOwnProps
+
+interface State {
+  text: String
+}
+
+class App extends React.Component<IProps, State> {
+  constructor(props: IProps) {
+    super(props)
+
+    this.state = {
+      text: 'Template'
+    }
+  }
+
+  render() {
+    const { text } = this.state
+    return <div className="container">{ text }</div>
+  }
+}
+
+function mapStateToProps(state: RootState): StateProps {
+  return {}
+}
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<RootState, null, AnyAction>): DispatchProps => {
+  return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+import logo from '../logo.svg'
+import './App.css'
+
+class App extends Component<any, any> {
+  render() {
+    return <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  }
+}
+
+
+function mapStateToProps(state: RootState): StateProps {
+  return {
+
+  }
+}
+
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<RootState, null, AnyAction>): DispatchProps => {
+  return {
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditInformation)
